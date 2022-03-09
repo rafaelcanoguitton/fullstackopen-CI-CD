aea74eb59082750a991cbc81669e74ca9a902e91
Rafael Cano <rafael.cano@ucsp.edu.pe>
2022-03-09 14:25:09 -0500

    🚑 adding healthcheck

--- controllers/testing.js
+++ controllers/testing.js
@@ -1,9 +1,12 @@
-const router=require('express').Router()
-const Blog=require('../models/blog')
-const User=require('../models/user')
-router.post('/reset',async(request,response)=>{
-    await User.deleteMany({})
-    await Blog.deleteMany({})
-    response.status(204).end()
-})
-module.exports = router
+const router = require("express").Router();
+const Blog = require("../models/blog");
+const User = require("../models/user");
+router.post("/reset", async (request, response) => {
+  await User.deleteMany({});
+  await Blog.deleteMany({});
+  response.status(204).end();
+});
+router.get("/healthcheck", (request, response) => {
+    response.status(200).send("<h1>Healthy</h1>").end();
+});
+module.exports = router;
